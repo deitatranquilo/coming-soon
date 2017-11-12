bc62f82070d34aba88de45981f6e7c5638d71322
Manu Farfaro <manufarfaro@gmail.com>
2017-11-12 18:57:07 -0300

    added afip fiscal data info link.

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -5,6 +5,9 @@ const Main = () => (
   <div className="main">
     <img src={isotypeImg} alt="deita tranquilo logo" />
     <div className="contact-links">
+      <a href="http://qr.afip.gob.ar/?qr=TUnsNQGdgEe5sdkbuin7tA,," target="_F960AFIPInfo">
+        <img className="icon-afip" src="https://www.afip.gob.ar/images/f960/DATAWEB.jpg" alt="Fiscal Data" border="0" />
+      </a>
       <a href="mailto:deitatranquilo@gmail.com" target="_blank" rel="noopener noreferrer">
         <i className="icon-mail" />
       </a>
