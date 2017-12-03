83c2aa8800bc519ef549c8c90b9bbad3c3f83567
Manu Farfaro <manufarfaro@gmail.com>
2017-12-03 15:31:14 -0300

    added accessibility support.

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import isotypeImg from '../../assets/images/isotype.png';
 
 const Main = () => (
-  <div className="main">
-    <img src={isotypeImg} alt="deita tranquilo logo" />
+  <main>
+    <img src={isotypeImg} alt="Deita Tranquilo Logo" />
     <div className="contact-links">
-      <a href="http://qr.afip.gob.ar/?qr=TUnsNQGdgEe5sdkbuin7tA,," target="_F960AFIPInfo">
+      <a href="http://qr.afip.gob.ar/?qr=TUnsNQGdgEe5sdkbuin7tA,," aria-label="Check our Fiscal Data" target="_F960AFIPInfo">
         <img className="icon-afip" src="https://www.afip.gob.ar/images/f960/DATAWEB.jpg" alt="Fiscal Data" border="0" />
       </a>
-      <a href="mailto:deitatranquilo@gmail.com" target="_blank" rel="noopener noreferrer">
+      <a href="mailto:deitatranquilo@gmail.com" aria-label="Contact us by Email" target="_blank" rel="noopener noreferrer">
         <i className="icon-mail" />
       </a>
-      <a href="https://www.facebook.com/deitatranquilo" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.facebook.com/deitatranquilo" aria-label="Visit us on Facebook" target="_blank" rel="noopener noreferrer">
         <i className="icon-facebook" />
       </a>
     </div>
-  </div>
+  </main>
 );
 
 export default Main;
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Style from './assets/stylesheets/main.scss';
+import './assets/stylesheets/main.scss';
 import Main from './components/Main';
 
 ReactDOM.render(<Main />, document.getElementById('root'));
